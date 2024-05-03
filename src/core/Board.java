d9c09aedf0475e88ce00a23d09e9a74583b84b53
zakaryan2107 <zakaryan2107@...>
2024-05-04 00:44:30 +0400

    Some Changes in the Board Class

--- src/core/Board.java
+++ src/core/Board.java
@@ -6,7 +6,7 @@ public class Board {
         this.board = new AbstractPiece[4][4]; 
     }
 
-    public boolean placePiece(int row, int col, AbstractPiece piece) {
+    public boolean placePiece (int row, int col, AbstractPiece piece) {
         if (isPositionValid(row, col) && isPositionEmpty(row, col)) {
             board[row][col] = piece;
             return true;
@@ -14,19 +14,19 @@ public class Board {
         return false;
     }
 
-    public boolean isPositionEmpty(int row, int col) {
+    public boolean isPositionEmpty (int row, int col) {
         return isPositionValid(row, col) && board[row][col] == null;
     }
-
-    private boolean isPositionValid(int row, int col) {
+ 
+    private boolean isPositionValid (int row, int col) {
         return row >= 0 && row < 4 && col >= 0 && col < 4;
     }
 
-    public boolean checkWin() {
+    public boolean checkWin () {
         return checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin();
     }
 
-    private boolean checkRowsForWin() {
+    private boolean checkRowsForWin () {
         for (int row = 0; row < 4; row++) {
             if (checkLineForWin(board[row][0], board[row][1], board[row][2], board[row][3])) {
                 return true;
@@ -35,7 +35,7 @@ public class Board {
         return false;
     }
 
-    private boolean checkColumnsForWin() {
+    private boolean checkColumnsForWin () {
         for (int col = 0; col < 4; col++) {
             if (checkLineForWin(board[0][col], board[1][col], board[2][col], board[3][col])) {
                 return true;
@@ -44,12 +44,12 @@ public class Board {
         return false;
     }
 
-    private boolean checkDiagonalsForWin() {
+    private boolean checkDiagonalsForWin () {
         return checkLineForWin(board[0][0], board[1][1], board[2][2], board[3][3]) ||
                checkLineForWin(board[0][3], board[1][2], board[2][1], board[3][0]);
     }
 
-    private boolean checkLineForWin(AbstractPiece... pieces) {
+    private boolean checkLineForWin (AbstractPiece... pieces) {
         if (pieces[0] == null) return false; // Can't have a line win if the first piece is null
         // Check each attribute type for matches
         for (Height height : Height.values()) {
@@ -67,12 +67,31 @@ public class Board {
         return false;
     }
 
-    private <T> boolean allMatchAttribute(AbstractPiece[] pieces, T attribute) {
-        return false;
+    private <T> boolean allMatchAttribute (AbstractPiece[] pieces, T attribute) {
+        for (AbstractPiece piece : pieces) {
+            if (piece == null || !isEqual(piece, attribute)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private <T> boolean isEqual (AbstractPiece piece, T attribute) {
+        if (attribute instanceof Height) {
+            return piece.getHeight() == attribute;
+        } else if (attribute instanceof Fullness) {
+            return piece.getFullness() == attribute;
+        } else if (attribute instanceof Form) {
+            return piece.getForm() == attribute;
+        } else if (attribute instanceof Color) {
+            return piece.getColor() == attribute;
+        } else {
+            return false;
+        }
     }
 
     @Override
-    public String toString() {
+    public String toString () {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
