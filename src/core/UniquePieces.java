6c691c0035a3539fd9005685638324eaaaf1560d
zakaryan2107 <zakaryan2107@...>
2024-05-03 16:43:23 +0400

    Adding class to hold all Pieces inside

---
+++ src/core/UniquePieces.java
@@ -0,0 +1,100 @@
+package core;
+
+public class UniquePieces {
+
+    public final class TallSolidSquareDarkPiece extends AbstractPiece {
+        public TallSolidSquareDarkPiece() {
+            super(Height.TALL, Fullness.SOLID, Form.SQUARE, Color.DARK);
+        }
+    }
+
+    public final class TallSolidSquareLightPiece extends AbstractPiece {
+        public TallSolidSquareLightPiece() {
+            super(Height.TALL, Fullness.SOLID, Form.SQUARE, Color.LIGHT);
+        }
+    }
+
+    public final class TallSolidRoundDarkPiece extends AbstractPiece {
+        public TallSolidRoundDarkPiece() {
+            super(Height.TALL, Fullness.SOLID, Form.ROUND, Color.DARK);
+        }
+    }
+
+    public final class TallSolidRoundLightPiece extends AbstractPiece {
+        public TallSolidRoundLightPiece() {
+            super(Height.TALL, Fullness.SOLID, Form.ROUND, Color.LIGHT);
+        }
+    }
+
+    public final class TallHollowSquareDarkPiece extends AbstractPiece {
+        public TallHollowSquareDarkPiece() {
+            super(Height.TALL, Fullness.HOLLOW, Form.SQUARE, Color.DARK);
+        }
+    }
+
+    public final class TallHollowSquareLightPiece extends AbstractPiece {
+        public TallHollowSquareLightPiece() {
+            super(Height.TALL, Fullness.HOLLOW, Form.SQUARE, Color.LIGHT);
+        }
+    }
+
+    public final class TallHollowRoundDarkPiece extends AbstractPiece {
+        public TallHollowRoundDarkPiece() {
+            super(Height.TALL, Fullness.HOLLOW, Form.ROUND, Color.DARK);
+        }
+    }
+
+    public final class TallHollowRoundLightPiece extends AbstractPiece {
+        public TallHollowRoundLightPiece() {
+            super(Height.TALL, Fullness.HOLLOW, Form.ROUND, Color.LIGHT);
+        }
+    }
+
+    public final class ShortSolidSquareDarkPiece extends AbstractPiece {
+        public ShortSolidSquareDarkPiece() {
+            super(Height.SHORT, Fullness.SOLID, Form.SQUARE, Color.DARK);
+        }
+    }
+
+    public final class ShortSolidSquareLightPiece extends AbstractPiece {
+        public ShortSolidSquareLightPiece() {
+            super(Height.SHORT, Fullness.SOLID, Form.SQUARE, Color.LIGHT);
+        }
+    }
+
+    public final class ShortSolidRoundDarkPiece extends AbstractPiece {
+        public ShortSolidRoundDarkPiece() {
+            super(Height.SHORT, Fullness.SOLID, Form.ROUND, Color.DARK);
+        }
+    }
+
+    public final class ShortSolidRoundLightPiece extends AbstractPiece {
+        public ShortSolidRoundLightPiece() {
+            super(Height.SHORT, Fullness.SOLID, Form.ROUND, Color.LIGHT);
+        }
+    }
+
+    public final class ShortHollowSquareDarkPiece extends AbstractPiece {
+        public ShortHollowSquareDarkPiece() {
+            super(Height.SHORT, Fullness.HOLLOW, Form.SQUARE, Color.DARK);
+        }
+    }
+
+    public final class ShortHollowSquareLightPiece extends AbstractPiece {
+        public ShortHollowSquareLightPiece() {
+            super(Height.SHORT, Fullness.HOLLOW, Form.SQUARE, Color.LIGHT);
+        }
+    }
+
+    public final class ShortHollowRoundDarkPiece extends AbstractPiece {
+        public ShortHollowRoundDarkPiece() {
+            super(Height.SHORT, Fullness.HOLLOW, Form.ROUND, Color.DARK);
+        }
+    }
+
+    public final class ShortHollowRoundLightPiece extends AbstractPiece {
+        public ShortHollowRoundLightPiece() {
+            super(Height.SHORT, Fullness.HOLLOW, Form.ROUND, Color.LIGHT);
+        }
+    }
+}
