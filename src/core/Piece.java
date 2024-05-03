1ed3db23824798a32d006586d4505b65cc4fb15b
zakaryan2107 <zakaryan2107@...>
2024-05-03 15:28:57 +0400

    Adding Piece Interface.

---
+++ src/core/Piece.java
@@ -0,0 +1,15 @@
+package core;
+
+public interface Piece {
+
+    Form getForm();
+
+    Color getColor();
+
+    Fullness getFullness();
+
+    Height getHeight();
+
+    String toString();
+    
+}
