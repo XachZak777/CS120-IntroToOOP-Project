1a6fc9f05ce55fba93deead2132334c878b5a2fc
zakaryan2107 <zakaryan2107@...>
2024-05-03 16:59:24 +0400

    Deleting the Unnesessary Classes

--- src/IllegalArrangementException.java
+++
@@ -1,9 +0,0 @@
-public class IllegalArrangementException extends Exception {
-    public IllegalArrangementException() {
-        super("Invalid arrangement of pieces on the board.");
-    }
-
-    public IllegalArrangementException(String message) {
-        super(message);
-    }
-}
